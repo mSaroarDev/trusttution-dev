cec880a5eaef3527a37cb9a8c1676d4901112391
mSaroarDev <msaroar.dev@gmail.com>
2025-10-17 01:47:23 +0600

    feat: add UserDropdown component for user profile actions in DashHeader

--- src/components/DashHeader.jsx
+++ src/components/DashHeader.jsx
@@ -1,11 +1,18 @@
+import UserDropdown from "./UserDropdown";
+import { IoWalletOutline } from "react-icons/io5";
+
 const DashHeader = ({ width }) => {
   return (
     <div
       style={{ marginLeft: width }}
-      className="fixed top-0 left-0 right-0 z-30 text-white h-16 flex items-center justify-end px-6 shadow-md"
+      className="fixed top-0 left-0 right-0 z-30 h-16 flex items-center justify-end px-6 shadow-md"
     >
-      <div>
-        
+      <div className="flex items-center gap-x-5">
+        <div className="flex items-center gap-2 mr-3">
+          <IoWalletOutline size={25} />
+          <span className="mt-1">$276</span>
+        </div>
+        <UserDropdown />
       </div>
     </div>
   );
